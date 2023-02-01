ceb587147616938aeaf651dfca3a6da4bc80f59b
Arthur-Olivier Fortin <aofor@ulaval.ca>
2023-02-01 09:27:31 -0500

    Début exercice 1

---
+++ Exercice1/exercice1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+void main(){
+
+    int a, b, c;
+
+    prinf("écrire la valeur de A\n");
+    scanf("%d", &a);
+
+    prinf("écrire la valeur de A\n");
+    scanf("%d", &b);
+
+    prinf("écrire la valeur de A\n");
+    scanf("%d", &c);
+
+    printf("%d","A =" a,"%d","B =" b,"%d","C =" c)
+    
+    a = b;
+    b = c;
+    c = a;
+
+    printf("Après permutation :""%d","A =" a,"%d","B =" b,"%d","C =" c)
+
+}
